9fd52208cbfb65075c333eb209bc5dfe1e3bf677
Jaclyn Beck <jaclyn.beck@sagebase.org>
2025-09-15 13:13:06 -0700

    Added Clu samples to genome benchmarking to test the addition of CLU to the reference genome

--- genome_benchmark_scripts/Benchmark_ChooseSamples.R
+++ genome_benchmark_scripts/Benchmark_ChooseSamples.R
@@ -1,13 +1,13 @@
 # This script picks samples that will be used to benchmark custom reference
 # genomes. Samples are selected from three studies: Jax.IU.Pitt_5XFAD,
-# Jax.IU.Pitt_APOE4.Trem2.R47H, and UCI_3xTg-AD, which cover all human
-# transgenes present in any MODEL-AD study. Samples were selected from female
-# mice that were approximately 12 months of age, allowing for differences in
-# sample availability:
+# Jax.IU.Pitt_APOE4.Trem2.R47H, UCI_3xTg-AD, and UCI_Clu-h2kbKI, which cover all
+# human transgenes present in any MODEL-AD study. Samples were selected from
+# female mice that were approximately 12 months of age, allowing for differences
+# in sample availability:
 #   3xTg: 12 months
 #   5XFAD: 11 months
-#   APOE4: 13-16 months, except for APOE4 noncarriers, where only 3-4 month-old
-#          mice are available
+#   APOE4: 13-16 months
+#   Clu: 12 months
 #
 # The scripts 01_Harmonize_Metadata.R and 02_NF_CreateSampleSheets.R must be run
 # before running this script.
@@ -26,23 +26,19 @@ metadata <- read.csv(file.path(tmp_dir, "Model_AD_merged_metadata.csv"))
 # Remove samples that have an NA genotype
 metadata <- subset(metadata, !is.na(genotype))
 
-# 5XFAD, APOE4, and 3xTg cover all the humanized genes in the custom reference genome
+# 5XFAD, APOE4, 3xTg, and Clu cover all the humanized genes in the custom reference genome
 bench <- subset(metadata, study_name %in% c("Jax.IU.Pitt_5XFAD",
                                             "Jax.IU.Pitt_APOE4.Trem2.R47H",
-                                            "UCI_3xTg-AD"))
+                                            "UCI_3xTg-AD",
+                                            "UCI_Clu-h2kbKI"))
 
 # Select female mice, and narrow down the ages to the ranges we want
 bench <- subset(bench, sex == "female" &
-                  round(ageDeath) %in% c(3, 4, 11, 12, 13, 14, 15, 16))
-bench <- subset(bench, (study_name == "UCI_3xTg-AD" & round(ageDeath) == 12) |
-                  (study_name == "Jax.IU.Pitt_5XFAD" & round(ageDeath) == 11) |
-                  (study_name == "Jax.IU.Pitt_APOE4.Trem2.R47H" & round(ageDeath) >= 13) |
-                  (genotype == "APOE4_noncarrier" & round(ageDeath) <= 4))
+                  round(ageDeath) %in% c(11, 12, 13, 14, 15, 16))
 
 # Get rid of some genotypes that only have 1-2 samples
 low_genotypes <- c("APOE4-KI_heterozygous; Trem2-R47H_homozygous",
-                   "APOE4-KI_homozygous; Trem2-R47H_heterozygous",
-                   "APOE4-KI_homozygous; Trem2-R47H_WT")
+                   "APOE4-KI_homozygous; Trem2-R47H_heterozygous")
 bench <- subset(bench, !(genotype %in% low_genotypes))
 
 # Check -- all specimen IDs should be unique
@@ -57,7 +53,7 @@ write.csv(bench,
 # studies, we can load them in and filter to just the benchmark samples
 lines <- lapply(unique(bench$study_name), function(study) {
   ss_name <- file.path("data", "sample_sheets",
-                       paste0(study, "_samplesheet_synapse.csv"))
+                       paste0(study, "_samplesheet_rnaseq.csv"))
   stopifnot(file.exists(ss_name))
 
   samples <- read.csv(ss_name)
@@ -68,5 +64,5 @@ lines <- lapply(unique(bench$study_name), function(study) {
 lines <- do.call(rbind, lines)
 
 write.csv(lines,
-          file.path("data", "sample_sheets", "genome_reference_samplesheet_synapse.csv"),
+          file.path("data", "sample_sheets", "genome_reference_samplesheet_rnaseq.csv"),
           row.names = FALSE, quote = FALSE)
